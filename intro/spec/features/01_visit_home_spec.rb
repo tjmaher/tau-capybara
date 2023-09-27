27dee6ea4e9d1ae57c7edbacace0e0e454fb4f6c
Grace Icay <mggicay@gmail.com>
2023-09-27 23:43:17 +0900

    Updated contents of the intro folder Fixed dependency issues Added tests to spec/features/ for test #01 and test#02

--- intro/spec/features/01_visit_home_spec.rb
+++ intro/spec/features/01_visit_home_spec.rb
@@ -18,17 +18,57 @@ feature 'Visit The-Internet Home Page' do
     expect(page).to have_current_path('https://the-internet.herokuapp.com')
   end
 
-  scenario 'Verify Link Exists: Form Authentication' do
+  scenario 'Verify Page Links Exist' do
+    expect(page).to have_link('A/B Testing')
+    expect(page).to have_link('Add/Remove Elements')
+    expect(page).to have_link('Basic Auth')
+    expect(page).to have_link('Broken Images')
+    expect(page).to have_link('Challenging DOM')
+    expect(page).to have_link('Checkboxes')
+    expect(page).to have_link('Context Menu')
+    expect(page).to have_link('Digest Authentication')
+    expect(page).to have_link('Disappearing Elements')
+    expect(page).to have_link('Drag and Drop')
+    expect(page).to have_link('Dropdown')
+    expect(page).to have_link('Dynamic Content')
+    expect(page).to have_link('Dynamic Controls')
+    expect(page).to have_link('Dynamic Loading')
+    expect(page).to have_link('Entry Ad')
+    expect(page).to have_link('Exit Intent')
+    expect(page).to have_link('File Download')
+    expect(page).to have_link('File Upload')
+    expect(page).to have_link('Floating Menu')
+    expect(page).to have_link('Forgot Password')
     expect(page).to have_link('Form Authentication')
+    expect(page).to have_link('Frames')
+    expect(page).to have_link('Geolocation')
+    expect(page).to have_link('Horizontal Slider')
+    expect(page).to have_link('Hovers')
+    expect(page).to have_link('Infinite Scroll')
+    expect(page).to have_link('Inputs')
+    expect(page).to have_link('JQuery UI Menus')
+    expect(page).to have_link('JavaScript Alerts')
+    expect(page).to have_link('JavaScript onload event error')
+    expect(page).to have_link('Key Presses')
+    expect(page).to have_link('Large & Deep DOM')
+    expect(page).to have_link('Multiple Windows')
+    expect(page).to have_link('Nested Frames')
+    expect(page).to have_link('Notification Messages')
+    expect(page).to have_link('Redirect Link')
+    expect(page).to have_link('Secure File Download')
+    expect(page).to have_link('Shadow DOM')
+    expect(page).to have_link('Shifting Content')
+    expect(page).to have_link('Slow Resources')
+    expect(page).to have_link('Sortable Data Tables')
+    expect(page).to have_link('Status Codes')
+    expect(page).to have_link('Typos')
+    expect(page).to have_link('WYSIWYG Editor')
   end
 
   scenario 'Click Link: Form Authentication ==> Navigate to /login' do
     click_link('Form Authentication')
     expect(page).to have_current_path('/login')
-  end
-
-  scenario 'Verify Link Exists: Dropdown' do
-    expect(page).to have_link('Dropdown')
+    expect(page).to have_content('Login Page')
   end
 
   scenario 'Click On: Dropdown ==> Navigate to /dropdown' do
